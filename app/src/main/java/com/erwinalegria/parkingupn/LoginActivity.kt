06b7ebbd33b3128869c3564f367fa2846c1880fb
Erwin Alegria <yeri.erwin1797@gmail.com>
2024-09-11 14:00:30 -0500

    jeje

--- app/src/main/java/com/erwinalegria/parkingupn/LoginActivity.kt
+++ app/src/main/java/com/erwinalegria/parkingupn/LoginActivity.kt
@@ -1,6 +1,14 @@
 package com.erwinalegria.parkingupn
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +24,23 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //Hace que el text view abra un link
+        val textView: TextView = findViewById(R.id.link_olvide_mi_contraseña)
+        val spannableString = SpannableString("Olvidé mi contraseña")
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://passwordreset.microsoftonline.com/")
+                )
+                startActivity(intent)
+            }
+        }
+        // Asigna el ClickableSpan al texto completo
+        spannableString.setSpan(clickableSpan, 0, spannableString.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.text = spannableString
+        textView.movementMethod = LinkMovementMethod.getInstance() // Habilita los clics en el TextView
+        ////////////////////////
     }
 }
